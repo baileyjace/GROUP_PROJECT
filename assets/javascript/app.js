3392c8ebce4da158bd39231a1da138351e2bbc4e
bsthomson <thomson.bs@gmail.com>
2018-06-28 20:01:50 -0500

    when you click the find recipe button it calls the api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,10 +8,26 @@ var config = {
 };
 firebase.initializeApp(config);
 
-$("findRecipe").on("click", function() {
+var getRecipe = function() {
+    var search = $("#recipe").val().trim();
+    var appid = "897772a2";
+    var appkey = "c193011b1550064d6ebf4a7adb2ac3e8";
+    var queryURL = "https://api.edamam.com/search?q="+search+"&app_id="+appid+"&app_key="+appkey;
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+            
+      })
+    .then(function(response) {
+        console.log(response);
+    });
+}
 
+$("#findRecipe").on("click", function(event) {
+    event.preventDefault();
+    getRecipe();
 })
 
-$("findRecipeIngredients").on("click", function() {
+$("#findRecipeIngredients").on("click", function() {
     
 })
