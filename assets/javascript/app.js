0f7e28826b8b80654e7423a5ee88b863b44f18a5
Jordan Horne <rjf.horne@gmail.com>
2018-06-30 12:02:21 -0500

    zomato is working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,66 @@ var config = {
     messagingSenderId: "984419410414"
 };
 firebase.initializeApp(config);
+
+var getRecipe = function() {
+    var search = $("#recipe").val().trim();
+    var appid = "897772a2";
+    var appkey = "c193011b1550064d6ebf4a7adb2ac3e8";
+    var queryURL = "https://api.edamam.com/search?q="+search+"&app_id="+appid+"&app_key="+appkey;
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+            
+      })
+    .then(function(response) {
+        console.log(response);
+    });
+};
+
+$("#findRecipe").on("click", function(event) {
+    event.preventDefault();
+    getRecipe();
+});
+
+$("#findRecipeIngredients").on("click", function() {
+    
+});
+
+$("#findRestaurants").on("click", function(event) {
+    console.log("insideClick")
+    event.preventDefault();
+    findRestaurants();
+});
+    //getRestaurant();
+
+
+    function findRestaurants() {
+        //first ajax call to get city ID from Zomato
+        var search = $("#restaurants").val().trim();
+        var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=" + search;
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            headers: {"user-key": "ad3e6676b39ae63041f68de59afcb7e3"},
+            dataType: "json",
+        })
+        .then(function(response){
+            console.log(response);
+            console.log(response.location_suggestions[0].id);
+        
+            //second call to get the actual info that I want using Zomato ID
+            var searchID = response.location_suggestions[0].id;
+            var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=" + searchID;
+            $.ajax({
+                url: queryURL,
+                method: "GET",
+                headers: {"user-key": "ad3e6676b39ae63041f68de59afcb7e3"},
+                dataType: "json",
+            })
+            .then(function(response2){
+                console.log(response2);
+        
+            });
+        });
+    };
+
