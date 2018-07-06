fa395a3b26dcd18395bb7fe827c6234800735c9c
Jordan Horne <rjf.horne@gmail.com>
2018-07-05 20:02:54 -0500

    working restaurant search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,32 @@ var config = {
 };
 firebase.initializeApp(config);
 
+$(document).ready(function(){
+
+var recipes = [
+    "Chicken", "Pork", "Beef", "Fish", "Pasta", "Mushroom", "Shrimp", "Tofu",
+    "Seitan",
+];
+
+var randomRecipe = function() {
+    
+    var randomChoice = Math.floor(Math.random() * recipes.length + 1);
+    var search = recipes[randomChoice];
+    var appid = "897772a2";
+    var appkey = "c193011b1550064d6ebf4a7adb2ac3e8";
+    var queryURL = "https://api.edamam.com/search?q="+search+"&app_id="+appid+"&app_key="+appkey;
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+            
+      })
+    .then(function(response) {
+        console.log(response.hits[Math.floor(Math.random() * 10)]);
+    });
+    
+};
+
+
 var getRecipe = function() {
     var search = $("#recipe").val().trim();
     var appid = "897772a2";
@@ -32,10 +58,12 @@ $("#findRecipeIngredients").on("click", function() {
     
 });
 
+
 $("#findRestaurants").on("click", function(event) {
     console.log("insideClick")
     event.preventDefault();
     findRestaurants();
+    console.log("hello");
 });
     //getRestaurant();
 
@@ -51,22 +79,64 @@ $("#findRestaurants").on("click", function(event) {
             dataType: "json",
         })
         .then(function(response){
+            console.log(search);
             console.log(response);
+            console.log(queryURL);
             console.log(response.location_suggestions[0].id);
         
             //second call to get the actual info that I want using Zomato ID
             var searchID = response.location_suggestions[0].id;
-            var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=" + searchID;
+            var query2URL = "https://developers.zomato.com/api/v2.1/search?entity_id=" + searchID + "&entity_type=city&collection_id=1&count=5";
             $.ajax({
-                url: queryURL,
+                url: query2URL,
                 method: "GET",
                 headers: {"user-key": "ad3e6676b39ae63041f68de59afcb7e3"},
                 dataType: "json",
             })
             .then(function(response2){
                 console.log(response2);
+                console.log(query2URL);
+
+                        // variables for the query responses we want
+        var name = response2.restaurants["0"].restaurant.name;
+        var location = response2.restaurants["0"].restaurant.location.address;
+        var cuisine = response2.restaurants["0"].restaurant.cuisines;
+        var userRating = response2.restaurants["0"].restaurant.user_rating.aggregate_rating;
+        var phoneNumber = response2.restaurants["0"].restaurant.phone_numbers;
+        
+        //for loop to iterate through the first 5 responses
+        var restaurantArray = response2.restaurants
+        $.each(restaurantArray, function() {
+            $.each(this, function (restaurant, index){
+            console.log(restaurant + '=' + index); 
+        
+
+        // appending the results to the table
+        $("#restaurant-table > tbody").append(
+        "<tr><td id='table-name'>" + name + 
+        "</td><td id='table-location'>" + location + 
+        "</td><td id='table-cuisine'>" + cuisine +  
+        "</td><td id='table-rating'>" + userRating + 
+        "</td><td id='table-phone'>" + phoneNumber + 
+        "</td></tr>");
         
             });
         });
+            });
+        });
     };
+})
+
+
+
+
+
+    
+
+
+
+
+    
+
+
 
