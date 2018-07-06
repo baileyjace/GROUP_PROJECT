e604aa3bb1745ee98089ddab04dfddf584085cf4
Jordan Horne <rjf.horne@gmail.com>
2018-07-05 20:11:04 -0500

    cleaned up JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,3 @@
-var config = {
-    apiKey: "AIzaSyB7p3GymKOtuclgt-wKoWudxNcGaIQR7pQ",
-    authDomain: "wtfshouldieat.firebaseapp.com",
-    databaseURL: "https://wtfshouldieat.firebaseio.com",
-    projectId: "wtfshouldieat",
-    storageBucket: "wtfshouldieat.appspot.com",
-    messagingSenderId: "984419410414"
-};
-firebase.initializeApp(config);
-
-$(document).ready(function(){
-
-var recipes = [
-    "Chicken", "Pork", "Beef", "Fish", "Pasta", "Mushroom", "Shrimp", "Tofu",
-    "Seitan",
-];
-
-var randomRecipe = function() {
-    
-    var randomChoice = Math.floor(Math.random() * recipes.length + 1);
-    var search = recipes[randomChoice];
-    var appid = "897772a2";
-    var appkey = "c193011b1550064d6ebf4a7adb2ac3e8";
-    var queryURL = "https://api.edamam.com/search?q="+search+"&app_id="+appid+"&app_key="+appkey;
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-            
-      })
-    .then(function(response) {
-        console.log(response.hits[Math.floor(Math.random() * 10)]);
-    });
-    
-};
-
-
-var getRecipe = function() {
-    var search = $("#recipe").val().trim();
-    var appid = "897772a2";
-    var appkey = "c193011b1550064d6ebf4a7adb2ac3e8";
-    var queryURL = "https://api.edamam.com/search?q="+search+"&app_id="+appid+"&app_key="+appkey;
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-            
-      })
-    .then(function(response) {
-        console.log(response);
-    });
-};
-
-$("#findRecipe").on("click", function(event) {
-    event.preventDefault();
-    getRecipe();
-});
-
-$("#findRecipeIngredients").on("click", function() {
-    
-});
-
-
 $("#findRestaurants").on("click", function(event) {
     console.log("insideClick")
     event.preventDefault();
@@ -120,12 +59,11 @@ $("#findRestaurants").on("click", function(event) {
         "</td><td id='table-phone'>" + phoneNumber + 
         "</td></tr>");
         
-            });
         });
-            });
+             });
         });
-    };
-})
+    });
+}
 
 
 
